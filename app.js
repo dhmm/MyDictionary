2f363ca1ac8e79618e447172006693edafab7c70
dhmm <dhmm@outlook.com>
2022-08-25 09:39:13 +0300

    code cleanup

--- app.js
+++ app.js
@@ -28,35 +28,5 @@ var Panel = {
   show: function() {},
   hide: function() {}
 }
-
-// var SelectedWord = {
-//   showSelection: function() {
-//     alert('ok');
-//   },
-//   getSelection : function(e) {
-//     var focused_window = document.commandDispatcher.focusedWindow;
-//     var sel_text = content.getSelection();
-//     alert(sel_text);
-
-//     //return sel_text.toString();  
-//   }
-// }
-
-// window.addEventListener("select", function(e) { SelectedWord.showSelection(); }, false);
 console.log('LOADED');
 Panel.init();
-
-// document.addEventListener("mouseup", function(event) {
-//   if(event.ctrlKey) {
-//   var text = window.getSelection().toString();
-//   alert(text);
-
-//   }
-// }, false);
-
-// const div1 = document.createElement('div');
-// div1.innerHTML = `<div style="display:block; position:fixed; z-index:99999; background:#123456; opacity:0.8; width:300px; height:100vh; top:0px; right:0px; ">Last element</div>`;
-
-
-// // ✅ Insert element as last in body
-// document.body.appendChild(div1);
